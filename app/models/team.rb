c9b3ca61ac5157960dc32fb428880d94a8e907f2
K-S-A <koniev.s.a@gmail.com>
2016-04-04 14:14:13 +0300

    Add relations to models.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,7 @@
 class Team < ActiveRecord::Base
+  has_and_belongs_to_many :users
+
+  def matches
+    Match.where('host_team_id = :team_id OR guest_team_id = :team_id', team_id: self.id)
+  end
 end
---
+++ db/schema.rb
@@ -0,0 +1,70 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160404101446) do
+
+  create_table "matches", force: :cascade do |t|
+    t.integer  "round_id"
+    t.integer  "host_score"
+    t.integer  "guest_score"
+    t.integer  "host_team_id",  null: false
+    t.integer  "guest_team_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "matches", ["round_id"], name: "index_matches_on_round_id"
+
+  create_table "rounds", force: :cascade do |t|
+    t.integer  "tournament_id"
+    t.string   "type",          default: "regular", null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+  end
+
+  add_index "rounds", ["tournament_id"], name: "index_rounds_on_tournament_id"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "team_id", null: false
+  end
+
+  add_index "teams_users", ["team_id", "user_id"], name: "index_teams_users_on_team_id_and_user_id"
+  add_index "teams_users", ["user_id", "team_id"], name: "index_teams_users_on_user_id_and_team_id"
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string   "name",                               null: false
+    t.string   "status",     default: "not started", null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email",      null: false
+    t.string   "img_link"
+    t.integer  "rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+
+end
