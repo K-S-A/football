af2ac4dd58d80e57ee6846c2660a0ba356f4a2f1
K-S-A <koniev.s.a@gmail.com>
2016-04-12 16:33:52 +0300

    Substitute raw-sql insert with AR.create.

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,5 @@
 class Round < ActiveRecord::Base
   belongs_to :tournament
   has_many :matches
+  has_and_belongs_to_many :teams
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -11,27 +11,15 @@ class Team < ActiveRecord::Base
       # get tournament participants
       users_teams = tournament_teams(tournament_id, team_size)
 
-      # generate teams_params for batch insert
+      # generate teams_params
       teams_params = users_teams.each.with_object([]) do |team, obj|
-        obj.push("('#{team.map(&:short_name).join(' + ')}',\
-                  #{tournament_id},\
-                  '#{Time.current}',\
-                  '#{Time.current}')")
+        obj.push(name: team.map(&:short_name).join(' + '),
+                 tournament_id: tournament_id,
+                 user_ids: team.map(&:id))
       end
 
-      # insert teams and get IDs of inserted teams
+      # insert teams/users
       teams = insert_teams(teams_params)
-
-      # insert HBTM users/teams relations
-      teams_users_params = teams.each.with_index.with_object([]) do |(team, index), obj|
-        users_teams[index].each do |user|
-          obj.push("(#{user.id}, #{team.id})")
-        end
-      end
-
-      insert_teams_users(teams_users_params)
-
-      teams
     end
 
     private
@@ -64,26 +52,13 @@ END_SQL
       groups.transpose
     end
 
-    # => Array of inserted Teams IDs
+    # => Array of inserted Teams
     def insert_teams(teams_params)
-      # TODO: sanitize params
-      query = <<END_SQL
-        INSERT INTO teams
-          (name, tournament_id, created_at, updated_at)
-        VALUES #{teams_params.join(', ')}
-        RETURNING teams.id, teams.name
-END_SQL
+      teams = Team.create(teams_params)
+      # preload :users
+      ActiveRecord::Associations::Preloader.new.preload(teams, :users)
 
-      Team.find_by_sql(query)
-    end
-
-    def insert_teams_users(teams_users_params)
-      query = <<END_SQL
-        INSERT INTO teams_users
-        VALUES #{teams_users_params.join(', ')}
-END_SQL
-
-      Team.find_by_sql(query)
+      teams
     end
   end
 end
