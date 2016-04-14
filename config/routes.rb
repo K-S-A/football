ba6b9bec5593a59b21875844d2886f2114c6b5b4
K-S-A <koniev.s.a@gmail.com>
2016-04-14 14:35:50 +0300

    Update routes for tournaments/rounds/teams.

--- config/routes.rb
+++ config/routes.rb
@@ -7,57 +7,14 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#main'
 
-  resources :tournaments, except: [:new, :edit], shallow: true do
-    resources :teams, except: [:new, :edit]
-    resources :rounds, except: [:new, :edit]
+  resources :tournaments, except: [:new, :edit] do
+    resources :teams, only: [:index, :create]
+    resources :rounds, only: [:index, :create, :show]
   end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :rounds, only: [:update, :destroy] do
+    resources :teams, only: [:index, :create]
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :teams, only: [:show, :update, :destroy]
 end
